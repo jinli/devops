a28902af5568460389ab24fdfd34c2d2544d5abe
Jin Li <jinlie@gmail.com>
2020-05-17 21:37:46 -0700

    JinL - ubuntu dev box docker files

---
+++ dockerfiles/ubuntu-dev-ssh/run.sh
@@ -0,0 +1,1 @@
+docker run -dit -p 3000:3000 -p 3001:3001 -p 2222:22 -v e:/projects:/home/jin/projects ubdev-ssh:1804 
---
+++ dockerfiles/ubuntu-dev/run.sh
@@ -0,0 +1,1 @@
+docker run -dit -p 3000:3000 -p 3001:3001 -v e:/projects:/projects ubdev:1804 
