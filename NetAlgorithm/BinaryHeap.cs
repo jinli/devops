866be05e8ae99ce105ba0321f6e67f53a975ccc7
Jin Li <jin.li@se.com>
2021-08-26 19:27:17 -0700

    JinL - net algorithm

---
+++ NetAlgorithm/BinaryHeap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCoreConsole
+{
+    //https://www.c-sharpcorner.com/article/binary-heap-in-c-sharp/
+    class BinaryHeap
+    {
+        public static void Test()
+        {
+            var ar = new[] { 1, 2, 3 };
+            Array.Sort(ar);
+        }
+    }
+}
---
+++ NetAlgorithm/DFS.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCoreConsole
+{
+    public class DFS
+    {
+        private int NumVertice;
+
+        private List<int>[] adj;
+
+        public DFS(int v)
+        {
+            NumVertice = v;
+            adj = new List<int>[v];
+            for (int i = 0; i < v; i++)
+                adj[i] = new List<int>();
+        }
+
+        public void AddEdge(int v, int w)
+        {
+            adj[v].Add(w);
+        }
+
+        public void DFSRecursive(int v, bool[] visited)
+        {
+            visited[v] = true;
+            Console.WriteLine(v + " ");
+
+            var vList = adj[v];
+            foreach(var n in vList)
+            {
+                if (!visited[n])
+                    DFSRecursive(n, visited);
+            }
+        }
+
+        public void Traverse(int start)
+        {
+            var visited = new bool[NumVertice];
+            DFSRecursive(start, visited);
+        }
+    }
+}
